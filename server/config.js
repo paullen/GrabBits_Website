044f72abd6d0b26a46362523a1a640057dcbc21f
paullen <bitanpaul1@gmail.com>
2023-05-24 01:17:44 +0530

    replace ethereal credentials with valid ones

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 export default{ 
     JWT_SECRET : "cZNBhV5EMOeJjqp4y3Of3JGVb0+DfMfUK6SeAiLmqwI=",
-    EMAIL: "gracie79@ethereal.email",
-    PASSWORD: "P5adXq1UFrNBKUUfWd"
+    EMAIL: 'wilma.hegmann@ethereal.email',
+    PASSWORD: 'crDRbe7ExnWRDKFSvQ'
 }
